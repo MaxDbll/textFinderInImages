e871476b8e8243b9da74bf8145902558d706d564
Maxence Duboille <maxence.duboille@ik.me>
2023-08-11 19:48:11 +0200

    Use tesseract added to data directory

--- index.py
+++ index.py
@@ -6,14 +6,34 @@ from PIL import Image
 from typing import List
 from rich.progress import track
 
+
+def get_data_file_path(relative_path):
+    # Obtient le chemin absolu du répertoire du script en cours d'exécution
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construit le chemin complet du fichier de données en utilisant le chemin du script et le chemin relatif
+    data_file_path = os.path.join(script_dir, relative_path)
+
+    return data_file_path
+
 # Get first param of the script if exists
-param: str = ""
+testMode: bool = False
+
 if len(sys.argv) > 1:
     param = sys.argv[1]
+    if param == "-test":
+        testMode = True
+
+# Set tesseract path
+if testMode:
+    # Here you need to set the path to tesseract.exe if it's not in the PATH environment variable
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+else:
+    # Find the path to tesseract.exe in the data directory
+    pytesseract.pytesseract.tesseract_cmd = get_data_file_path('tesseract.exe') # Path to tesseract.exe
 
 # if param is empty, ask the user for a directory path
-if param == "":
-    param = input("Entrez un chemin de répertoire : ")
+param: str = input("Entrez un chemin de répertoire : ")
 
 # Check if param is a directory path
 if not os.path.isdir(param):
@@ -56,7 +76,6 @@ for file in track(files, description="Traitement des fichiers"):
 # Check if there is any result
 if len(res) == 0:
     print("Aucune correspondance trouvée")
-    exit()
 
 # Ask the user to press enter
 input("Appuyez sur entrée pour terminer...")
